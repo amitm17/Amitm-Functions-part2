dfff8ea008d1623e26e68479094a1df691803ab1
amitm17 <messasamit17@gmail.com>
2024-04-09 13:48:46 +0300

    initial commit, base code from exc

---
+++ FunctionExTwo/FunctionExTwo/FunctionExTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FunctionExTwo.Abstract;
+
+namespace FunctionExTwo
+{
+    public class FunctionExTwo : IExerciseTwo
+    {
+        public bool IsFromFibonacci(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPalindrom(int number)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public int RecursivePower(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
